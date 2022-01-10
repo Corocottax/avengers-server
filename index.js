c96e7ba4b6a9071c7bb01ea82f40d82ea4636f73
unknown <santicorocottax@gmail.com>
2022-01-10 11:27:06 +0100

    rama develop commit 1

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ const PORT = 5000;
 
 const server = express();
 
-server.use("/songoku", (req, res) => {
+server.use("/v1/pepe", (req, res) => {
+
+    res.send("Hola pepe");
+
+});
+
+server.use("/v1/songoku", (req, res) => {
 
     res.send("Hola songoku");
 
 });
 
-server.use("/koi", (req, res) => {
+server.use("/v1/koi", (req, res) => {
 
     res.send("El mejor equipo del mundo");
 
